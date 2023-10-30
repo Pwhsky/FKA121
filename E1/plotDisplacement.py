59d50b4237e0cb496d0fb445066c873d79e2cfc8
Pwhsky <lechal.pierre@gmail.com>
2023-10-30 09:58:43 +0100

    Maybe working solution? ask the TA

--- E1/plotDisplacement.py
+++ E1/plotDisplacement.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 array = np.genfromtxt('coupled_oscillator.csv',delimiter=',',skip_header=1)
 spectrum = np.genfromtxt('powerspectrum.csv',delimiter=',',skip_header=1)
-fig,ax = plt.subplots(3,1,figsize=(7,7))
+fig,ax = plt.subplots(3,1,figsize=(10,10))
 ax[0].plot(array[:,0],array[:,1])
 ax[0].plot(array[:,0],array[:,2])
 ax[0].plot(array[:,0],array[:,3])
 ax[0].set_ylabel('Displacement (Å)')
 ax[0].set_xlabel('time (ps)')
+ax[0].legend(["O","C", "O"])
 ax[0].grid()
 
 ax[1].plot(array[:,0],array[:,4])
@@ -16,13 +17,16 @@ ax[1].plot(array[:,0],array[:,5])
 ax[1].plot(array[:,0],array[:,6])
 ax[1].set_ylabel('Energy (eV)')
 ax[1].set_xlabel('time (ps)')
+ax[1].legend(["Ekin","Epot", "Etot"])
 ax[1].grid()
 
-spectrumSum = spectrum[:, 0]+spectrum[:, 1]+spectrum[:, 2]
+spectrumSum = np.array(spectrum[:, 0]+spectrum[:, 1]+spectrum[:, 2])
+spectrum = np.array(spectrum)*33.3564095
 ax[2].plot(spectrum[:,3],spectrumSum )
 ax[2].set_ylabel('Energy (eV)')
-ax[2].set_xlabel('frequency (Thz)')
-ax[2].set_xlim(-200,200)
+ax[2].set_xlabel('wavenumber (cm^-1)')
+ax[2].set_xlim(-5000,5000)
+
 ax[2].grid()
 
 
