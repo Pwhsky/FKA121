9b91d5665de3d801623a0bcb638d930224aecfc7
Pwhsky <lechal.pierre@gmail.com>
2023-11-01 21:55:04 +0100

    finished C4 fourier-transform

--- C4/fourier_transform/plotSignal.py
+++ C4/fourier_transform/plotSignal.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+pi = 3.14159
+N = 250
+dt = 0.1
+time = np.arange(0,250)*dt
 
-array = np.genfromtxt('signal.csv',delimiter=',',skip_header=1)
+signal = np.cos(time*2*pi*2 + pi/2)
+#signal = np.cos(time*2*pi*2 + 0) + np.cos(2*pi*6*time)
 fig,ax = plt.subplots()
-ax.plot(array[:,0],array[:,1])
+ax.plot(time,signal)
 ax.set_xlabel('time (arb.unit)')
 ax.set_ylabel('signal (arb.unit)')
 ax.grid()
--- C4/fourier_transform/plotSpectrum.py
+++ C4/fourier_transform/plotSpectrum.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-array = np.genfromtxt('powerspectrum.csv',delimiter=',',skip_header=1)
+pi = 3.14159
+N = 255
+dt = 0.1
+time = np.arange(0,N)*dt
+freq = np.fft.fftfreq(len(time),dt)
+freq = np.fft.fftshift(freq)
+#signal = np.cos(time*2*pi*2 + pi/2)
+signal  = np.cos(time*2*pi*2 + 0) + np.cos(2*pi*6*time)
+fourier_transform = np.fft.fft(signal)
 fig,ax = plt.subplots()
-ax.plot(array[:,1],array[:,0])
+ax.plot(freq,np.abs(fourier_transform))
 ax.set_xlabel('freq (arb.unit)')
 ax.set_ylabel('signal (arb.unit)')
 ax.grid()
-fig.savefig('powerspectrum.pdf')
+fig.savefig('spectrum.pdf')
